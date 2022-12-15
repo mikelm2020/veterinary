61f9bd2d7278fc5d42aad0452dcbc73c4f2a3bdc
Miguel Angel López <admon@tecnott.com.mx>
2022-12-15 01:05:50 -0600

    Added login viewset

--- src/veterinary/apps/users/api/api.py
+++ src/veterinary/apps/users/api/api.py
@@ -3,22 +3,21 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from apps.users.api.serializers import UserLoginSerializer, UserModelSerializer
 from apps.users.models import User
+from rest_framework.permissions import AllowAny
+
 
 class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserModelSerializer
+    permission_classes = [AllowAny]
 
     @action(detail=False, methods=["post"])
     def login(self, request):
         """
-        user sing in
+        User sing in
         """
-        import pdb; pdb.set_trace()
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
-        data={
-            'user': UserModelSerializer(user),
-            'access_token': token
-        }
+        data = {"user": UserModelSerializer(user).data, "access_token": token}
         return Response(data, status=status.HTTP_201_CREATED)
--- src/veterinary/apps/users/api/serializers.py
+++ src/veterinary/apps/users/api/serializers.py
@@ -4,35 +4,6 @@ from rest_framework.authtoken.models import Token
 from apps.users.models import User
 
 
-class UserLoginSerializer(serializers.Serializer):
-        username = serializers.CharField(max_length=10)
-        password = serializers.CharField(min_length=8, max_length=64)
-
-        # def validate_username(self, value):
-        #     if len(value) > 10:
-        #         raise serializers.ValidationError("El username debe tener maximo 10 caracteres")
-        #     return value
-
-        # def validate_password(self, value):
-        #     if len(value) < 8:
-        #         raise serializers.ValidationError("El password debe tener como minimo 8 caracteres")
-        #     if len(value) > 64:
-        #         raise serializers.ValidationError("El password debe tener como máximo 64 caracteres")
-        #     return value
-
-        def validate(self, data):
-            user = authenticate(username=data["username"], password=data["password"])
-            if not user:
-                raise serializers.ValidationError("Las credenciales no son vaĺidas")
-
-            self.context["user"] = user
-            return data
-
-        def create(self, data):
-            token, created = Token.objects.get_or_create(user=self.context["user"])
-            return self.context["user"], token.key
-
-
 class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -45,3 +16,32 @@ class UserModelSerializer(serializers.ModelSerializer):
             "professional_license",
             "user_type",
         )
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=10)
+    password = serializers.CharField(min_length=8, max_length=64)
+
+    # def validate_username(self, value):
+    #     if len(value) > 10:
+    #         raise serializers.ValidationError("El username debe tener maximo 10 caracteres")
+    #     return value
+
+    # def validate_password(self, value):
+    #     if len(value) < 8:
+    #         raise serializers.ValidationError("El password debe tener como minimo 8 caracteres")
+    #     if len(value) > 64:
+    #         raise serializers.ValidationError("El password debe tener como máximo 64 caracteres")
+    #     return value
+
+    def validate(self, data):
+        user = authenticate(username=data["username"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Las credenciales no son vaĺidas")
+
+        self.context["user"] = user
+        return data
+
+    def create(self, data):
+        token, created = Token.objects.get_or_create(user=self.context["user"])
+        return self.context["user"], token.key
